3d893a3e48556834e9a5408cbea5044502689ef2
Rihards Zauls <rihards.zauls@gmail.com>
2022-11-13 14:07:00 +0200

    more amazing keybinds

--- nvim/lua/elite.lua
+++ nvim/lua/elite.lua
@@ -24,8 +24,13 @@ require('lspconfig').sumneko_lua.setup{}
 -- Go
 require('lspconfig').gopls.setup{
     on_attach = function ()
-        vim.keymap.set("n", "K", vim.lsp.buf.hover) -- peak definition
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition) -- go to definition
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0}) -- peak definition
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0}) -- go to definition
+        vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0}) -- go to definition
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0}) -- go to definition
+        vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, {buffer=0}) -- go to definition
+        vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, {buffer=0}) -- go to definition
+        vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", {buffer=0})
     end,
 }
 
