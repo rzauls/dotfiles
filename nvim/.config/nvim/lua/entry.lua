cd5abb69e3de5d2ad3885e8e6c504b703511cfc8
Rihards Zauls <rihards.zauls@gmail.com>
2022-11-15 22:27:14 +0200

    move telescope keybinds to telescope.lua

--- nvim/.config/nvim/lua/entry.lua
+++ nvim/.config/nvim/lua/entry.lua
@@ -1,36 +1,12 @@
 -- main config
-local ts_builtins = require('x.telescope') -- telescope config
 require('x.util') -- helper functions to debug config development
 require('x.plugins') -- packer and plugin list
+require('x.telescope') -- telescope config
 require('x.cmp') -- autocomplete/cmp config
 require('x.lsp') -- lsp config
 require('x.debug') -- debugger/dap config
 require('x.misc_configs') -- short configs that dont fit anywhere else
 
--- Exported keybind functions
--- (currently no reason to do this, since we dont use them in init.vim anymore)
-local opts = require('telescope.themes').get_dropdown({ winblend = 10 }) -- set theme override for the telescope calls
-local exported_mappings = {
-    -- switch buffers
-    buffers = function()
-        ts_builtins.buffers(opts)
-    end,
-    -- find files in cwd
-    find_files = function() -- find files in cwd
-        ts_builtins.find_files({ hidden = true, opts })
-    end,
-    -- fuzzy find in current buffer
-    current_buffer_fuzzy_find = function()
-        -- FIXME: this currently searches in the "previous" not current buffer for some reason
-        ts_builtins.current_buffer_fuzzy_find(opts, { buffer = 0 })
-    end,
-}
-
--- Generic global keybinds
-vim.keymap.set("n", "<C-p>", exported_mappings.find_files)
-vim.keymap.set("n", "<Leader><Tab>", exported_mappings.buffers)
-vim.keymap.set("n", "<Leader>ff", exported_mappings.current_buffer_fuzzy_find)
 vim.keymap.set("n", "<Leader>h", "<cmd>bprev<cr>") -- previous buffer
 vim.keymap.set("n", "<Leader>l", "<cmd>bnext<cr>") -- next buffer
 
-return exported_mappings
--- nvim/.config/nvim/lua/x/telescope.lua
+++ nvim/.config/nvim/lua/x/telescope.lua
@@ -1,22 +1,16 @@
-local action_state = require('telescope.actions.state')
 local ts_builtins = require('telescope.builtin')
 
 -- Telescope setup
-require('telescope').setup({
-    defaults = {
-        prompt_prefix = "> ",
-        mappings = {
-            -- insert mode mappings while in telescope prompt
-            i = {
-                ["<c-a>"] = function() print(vim.inspect(action_state.get_selected_entry())) end
-            }
-        }
-    }
-})
--- Load Telescope extensions
+require('telescope').setup()
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('file_browser')
 
+local theme_opts = require('telescope.themes').get_dropdown() -- set theme override for the telescope calls
+-- ts_builtins.find_files({ hidden = true, opts })
+-- ts_builtins.current_buffer_fuzzy_find(opts, { buffer = f })
+
+vim.keymap.set("n", "<C-p>", function() ts_builtins.find_files({ theme_opts }) end)
+vim.keymap.set("n", "<Leader><Tab>", function() ts_builtins.buffers({ theme_opts }) end)
+vim.keymap.set("n", "<Leader>ff", function() ts_builtins.current_buffer_fuzzy_find({ theme_opts }) end)
+
 return ts_builtins
-
-
