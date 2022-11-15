7685c3ec02c39cf01ea7aca7bb40909b312101aa
Rihards Zauls <rihards.zauls@gmail.com>
2022-11-15 22:28:09 +0200

    remove unused keybind

--- nvim/.config/nvim/lua/entry.lua
+++ nvim/.config/nvim/lua/entry.lua
@@ -7,6 +7,5 @@ require('x.lsp') -- lsp config
 require('x.debug') -- debugger/dap config
 require('x.misc_configs') -- short configs that dont fit anywhere else
 
-vim.keymap.set("n", "<Leader>h", "<cmd>bprev<cr>") -- previous buffer
-vim.keymap.set("n", "<Leader>l", "<cmd>bnext<cr>") -- next buffer
+vim.keymap.set("n", "<Leader>h", "<cmd>bprev<cr>") -- cycle to previous buffer
 
