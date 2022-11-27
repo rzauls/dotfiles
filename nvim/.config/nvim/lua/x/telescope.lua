63d4b2947438d65acae8dccdbc26f3efb984b6a2
Rihards Zauls <rihards.zauls@gmail.com>
2022-11-27 22:20:20 +0200

    add file browser keybind

--- nvim/.config/nvim/lua/x/telescope.lua
+++ nvim/.config/nvim/lua/x/telescope.lua
@@ -8,6 +8,13 @@ require('telescope').load_extension('file_browser')
 -- ts_builtins.current_buffer_fuzzy_find(opts, { buffer = f })
 
 vim.keymap.set("n", "<C-p>", function() ts_builtins.find_files({ hidden = true }) end)
+vim.keymap.set("n",
+    "<Leader><C-p>",
+    function()
+        local opts = require('telescope.themes').get_dropdown()
+        opts.hidden = true
+        require('telescope').extensions.file_browser.file_browser(opts)
+    end)
 vim.keymap.set("n", "<Leader><Tab>", function() ts_builtins.buffers() end)
 vim.keymap.set("n", "<Leader><Up>", function() ts_builtins.command_history() end)
 vim.keymap.set(
