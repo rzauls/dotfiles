2377156c6c4472c87e15e86b0779018768ffc476
Rihards Zauls <rihards.zauls@gmail.com>
2022-12-04 10:51:12 +0200

    remove cmp border, add scroll-view centering

--- nvim/.config/nvim/lua/x/keymap.lua
+++ nvim/.config/nvim/lua/x/keymap.lua
@@ -2,8 +2,10 @@
 -- TODO: map movement between buffers better
 vim.keymap.set("n", "<Leader>h", "<cmd>bprev<cr>") -- cycle to previous buffer
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz") -- ccenter view when scrolling by half page
+vim.keymap.set("n", "<C-u>", "<C-u>zz") -- center view when scrolling by half page
 
-vim.keymap.set("n", "<Leader><C-e>", "<cmd>NvimTreeToggle<cr>") --toggle file tree 
+vim.keymap.set("n", "<Leader><C-e>", "<cmd>NvimTreeToggle<cr>") --toggle file tree
 --
 -- TODO: figure out a vertical/horizontal split flow
 -- TODO: map movements between splits to something reasonable
