aede5e4eb23cc51429d3cd311af7988fce75a9fb
Rihards Zauls <rihards.zauls@gmail.com>
2024-03-15 13:42:58 +0200

    fix go.lua return type

--- nvim/.config/nvim/lua/custom/plugins/go.lua
+++ nvim/.config/nvim/lua/custom/plugins/go.lua
@@ -1,3 +1,5 @@
 -- Golang specific stuff
 vim.filetype.add({ extension = { templ = "templ" } })
 vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = vim.lsp.buf.format })
+
+return {}
