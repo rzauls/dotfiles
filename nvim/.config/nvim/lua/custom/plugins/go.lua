fdaf9d59aeddc727406054863ab47d2f9c658ed0
Rihards Zauls <rihards.zauls@gmail.com>
2024-03-30 17:33:29 +0200

    fix wezterm config location remove ray-x/go.nvim

--- nvim/.config/nvim/lua/custom/plugins/go.lua
+++ nvim/.config/nvim/lua/custom/plugins/go.lua
@@ -158,17 +158,4 @@ vim.filetype.add({ extension = { templ = "templ" } })
 -- 	attach_to_buffer(vim.api.nvim_get_current_buf(), { "go", "test", "-v", "-json", [[./...]] })
 -- end, {})
 
-return {
-	"ray-x/go.nvim",
-	dependencies = { -- optional packages
-		"ray-x/guihua.lua",
-		"neovim/nvim-lspconfig",
-		"nvim-treesitter/nvim-treesitter",
-	},
-	config = function()
-		require("go").setup()
-	end,
-	event = { "CmdlineEnter" },
-	ft = { "go", "gomod" },
-	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-}
+return {}
