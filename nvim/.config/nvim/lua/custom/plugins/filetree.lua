4bc8df8d360b681dfdade686de80ceca434bfcf4
Rihards Zauls <rihards.zauls@gmail.com>
2024-03-30 20:26:35 +0200

    show hidden files in neotree

--- nvim/.config/nvim/lua/custom/plugins/filetree.lua
+++ nvim/.config/nvim/lua/custom/plugins/filetree.lua
@@ -11,6 +11,20 @@ return {
 	config = function()
 		require("neo-tree").setup({
 			close_if_last_window = true,
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = true,
+					hide_by_name = {
+						".github",
+						".DS_Store",
+						".vscode",
+						".idea",
+						"thumbs.db",
+					},
+					never_show = { ".git" },
+				},
+			},
 		})
 
 		vim.keymap.set("n", "<C-e>", function()
