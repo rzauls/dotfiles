2a813692136c861794d51ecb47495758989f6cac
Rihards Zauls <rihards.zauls@gmail.com>
2024-03-31 10:30:50 +0300

    change nvim colors

--- nvim/.config/nvim/lua/custom/plugins/theme.lua
+++ nvim/.config/nvim/lua/custom/plugins/theme.lua
@@ -1,20 +1,22 @@
 -- Color theme
 return {
 	{
-		"catppuccin/nvim",
+		"rebelot/kanagawa.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			vim.cmd.colorscheme("kanagawa")
+		end,
+	},
+	{
+		"catppuccin/nvim",
 		opts = {
 			flavour = "mocha",
 		},
 		config = function()
-			vim.cmd.colorscheme("catppuccin")
-			-- Configure highlights
-			vim.cmd.hi("Comment gui=none")
+			-- vim.cmd.colorscheme("catppuccin")
+			-- -- Configure highlights
+			-- vim.cmd.hi("Comment gui=none")
 		end,
-	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
 	},
 }
