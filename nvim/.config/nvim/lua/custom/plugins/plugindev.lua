eb63c13bc620c9a396720131d12da6512164078d
Rihards Zauls <rihards.zauls@gmail.com>
2024-04-07 11:20:37 +0300

    nvim - add keymap for lua testing

--- nvim/.config/nvim/lua/custom/plugins/plugindev.lua
+++ nvim/.config/nvim/lua/custom/plugins/plugindev.lua
@@ -1,4 +1,4 @@
--- helper functions for lua and lua plugin development
+-- Helper functions for lua and lua plugin development
 
 ---Print a human-readable representation of v and return it afterwards
 P = function(v)
@@ -15,5 +15,10 @@ R = function(name)
 	RELOAD(name)
 	return require(name)
 end
+
+-- Lua testing keymaps
+vim.keymap.set("n", "<leader>lt", function()
+	require("plenary.test_harness").test_file(vim.api.nvim_buf_get_name(0))
+end, { desc = "Plenary: Run current [l]ua [t]est file" })
 
 return {}
