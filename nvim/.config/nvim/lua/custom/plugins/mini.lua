4def0853285c189692b54ef8e41d91656d7e4863
Rihards Zauls <rihards.zauls@gmail.com>
2024-04-09 20:47:06 +0300

    nvim - re-add autopairs

--- nvim/.config/nvim/lua/custom/plugins/mini.lua
+++ nvim/.config/nvim/lua/custom/plugins/mini.lua
@@ -29,6 +29,6 @@ return {
 
 		-- Autopairs for paired-symbols (brackets, quotes etc)
 		-- NOTE: disabling this to force learn more mini.ai actions
-		-- require("mini.pairs").setup()
+		require("mini.pairs").setup()
 	end,
 }
