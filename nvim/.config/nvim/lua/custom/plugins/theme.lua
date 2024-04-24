ceb605c6c9d667bb1032074cef60278c01decc23
Rihards Zauls <rihards.zauls@gmail.com>
2024-04-24 22:28:07 +0300

    nvim - change colors once again

--- nvim/.config/nvim/lua/custom/plugins/theme.lua
+++ nvim/.config/nvim/lua/custom/plugins/theme.lua
@@ -1,12 +1,13 @@
 -- Color theme
+vim.cmd.hi("Comment gui=none")
+
 return {
 	{
 		"rebelot/kanagawa.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			vim.cmd.colorscheme("kanagawa")
-			vim.cmd.hi("Comment gui=none")
+			vim.cmd.colorscheme("kanagawa-dragon")
 		end,
 	},
 	{
@@ -16,7 +17,6 @@ return {
 		},
 		config = function()
 			-- vim.cmd.colorscheme("catppuccin")
-			-- vim.cmd.hi("Comment gui=none")
 		end,
 	},
 }
