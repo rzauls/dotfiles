06dcda72e50b0b13227ea59c509399610054c29a
Rihards Zauls <rihards.zauls@gmail.com>
2024-04-24 22:48:32 +0300

    nvim - remove old theme

--- nvim/.config/nvim/lua/custom/plugins/theme.lua
+++ nvim/.config/nvim/lua/custom/plugins/theme.lua
@@ -1,22 +1,11 @@
 -- Color theme
-vim.cmd.hi("Comment gui=none")
-
 return {
 	{
 		"rebelot/kanagawa.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
+		lazy = false, -- load during startup since this is main colorscheme
+		priority = 1000, -- load this before all the other start plugins
 		config = function()
 			vim.cmd.colorscheme("kanagawa-dragon")
-		end,
-	},
-	{
-		"catppuccin/nvim",
-		opts = {
-			flavour = "mocha",
-		},
-		config = function()
-			-- vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 }
