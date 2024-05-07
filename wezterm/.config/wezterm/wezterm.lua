c6e5192cf73be1c54cc02abec8c6630b70945df8
Rihards Zauls <rihardsza@D7050L-ox.Dlink>
2024-05-07 20:50:05 +0300

    wezterm - fix some mac issues

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,9 +6,9 @@ local is_windows = function()
 	return wezterm.target_triple:find("windows") ~= nil
 end
 
--- local is_mac = function()
--- 	return wezterm.target_triple:find("darwin") ~= nil
--- end
+local is_mac = function()
+	return wezterm.target_triple:find("darwin") ~= nil
+end
 
 if is_windows() then
 	config.default_domain = "WSL:Ubuntu"
@@ -18,13 +18,19 @@ end
 config.color_scheme = "Kanagawa (Gogh)"
 
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
 config.window_padding = {
 	left = 0,
 	right = 0,
 	top = 0,
 	bottom = 0,
 }
+
+if is_mac() then
+	-- config.window_decorations = "TITLE | RESIZE"
+	config.native_macos_fullscreen_mode = true
+else
+	config.window_decorations = "RESIZE"
+end
 
 -- Maximize window on launch
 wezterm.on("gui-startup", function()
