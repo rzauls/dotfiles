e133afa376497192bc8849522417bbf647e6df7a
Rihards Zauls <rihards.zauls@lmt.lv>
2024-07-15 10:11:53 +0300

    nvim - hide whitch-key launch notification

--- nvim/.config/nvim/lua/custom/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/custom/plugins/whichkey.lua
@@ -3,7 +3,9 @@ return {
 	"folke/which-key.nvim",
 	event = "VimEnter", -- Sets the loading event to 'VimEnter'
 	config = function() -- This is the function that runs, AFTER loading
-		require("which-key").setup()
+		require("which-key").setup({
+			notify = false,
+		})
 
 		-- Document existing key chains
 		require("which-key").register({
