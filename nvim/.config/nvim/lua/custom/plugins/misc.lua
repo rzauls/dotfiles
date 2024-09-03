48e31eba4c6c887745e964c8901fb7d680e8bf94
Rihards Zauls <rihards.zauls@lmt.lv>
2024-09-03 09:54:22 +0300

    nvim - fix netrw file copy on mac

--- nvim/.config/nvim/lua/custom/plugins/misc.lua
+++ nvim/.config/nvim/lua/custom/plugins/misc.lua
@@ -3,4 +3,5 @@ return {
 	{ "tpope/vim-sleuth" }, -- Detect tabstop and shiftwidth automatically
 	{ "numToStr/Comment.nvim", opts = {} }, -- "gc" to comment visual regions/lines
 	{ "j-hui/fidget.nvim", opts = {} }, -- Loaders/progress bars
+	{ "dzirtusss/netrw_keepdir_fix" }, -- Fix netrw 'mc' on mac
 }
