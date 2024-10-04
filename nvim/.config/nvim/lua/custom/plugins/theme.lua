9b1ac192323322fc8c35a51b4676ed0a22a72ea2
Rihards Zauls <rihards.zauls@lmt.lv>
2024-10-04 15:03:54 +0300

    nvim - update deps

--- nvim/.config/nvim/lua/custom/plugins/theme.lua
+++ nvim/.config/nvim/lua/custom/plugins/theme.lua
@@ -5,7 +5,14 @@ return {
 		lazy = false, -- load during startup since this is main colorscheme
 		priority = 1000, -- load this before all the other start plugins
 		config = function()
-			vim.cmd.colorscheme("kanagawa-dragon")
+			-- vim.cmd.colorscheme("kanagawa-dragon")
+		end,
+	},
+	{
+		"savq/melange-nvim",
+		config = function()
+			vim.opt.termguicolors = true
+			vim.cmd.colorscheme("melange")
 		end,
 	},
 	{
