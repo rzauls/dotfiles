8ed0a19806fde272d88e8e1c3aa3af0cde09513a
Rihards Zauls <rihards.zauls@lmt.lv>
2024-12-12 15:48:15 +0200

    nvim - change lualine sections

--- nvim/.config/nvim/lua/custom/plugins/theme.lua
+++ nvim/.config/nvim/lua/custom/plugins/theme.lua
@@ -16,6 +16,27 @@ return {
 				options = {
 					theme = "tokyonight",
 				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = {
+						{
+							"filename",
+							path = 1,
+						},
+					},
+					lualine_x = { "encoding" },
+					lualine_y = { "filetype" },
+					lualine_z = { "location" },
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = { "filename" },
+					lualine_x = { "location", "fileformat" },
+					lualine_y = { "progress" },
+					lualine_z = {},
+				},
 			})
 		end,
 	},
