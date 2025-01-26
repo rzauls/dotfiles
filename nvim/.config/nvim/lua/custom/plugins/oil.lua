365c68958a38e4c29dbf94fcfd0efa6e55f9bfa9
Rihards Zauls <rihards.zauls@gmail.com>
2025-01-26 12:13:44 +0200

    nvim - show hidden files in oil.nvim

--- nvim/.config/nvim/lua/custom/plugins/oil.lua
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -4,7 +4,11 @@ return {
 	---@type oil.SetupOpts
 	opts = {},
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "[O]il up the parent directory" })
 		vim.keymap.set("n", "<c-e>", "<CMD>Oil<CR>", { desc = "[O]il up the parent directory" })
 	end,
