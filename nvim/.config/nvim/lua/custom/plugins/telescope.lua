2ffa0f0343cf46ef4dd1ea3eb1c10dfe0ba940f6
Rihards Zauls <rihards.zauls@lmt.lv>
2025-01-31 09:05:58 +0200

    nvim - replace unused keybind

--- nvim/.config/nvim/lua/custom/plugins/telescope.lua
+++ nvim/.config/nvim/lua/custom/plugins/telescope.lua
@@ -56,7 +56,7 @@ return {
 		map("<leader>sc", builtin.commands, { desc = "[s]earch [c]ommands" })
 		map("<leader>sk", builtin.keymaps, { desc = "[s]earch [k]eymaps" })
 		map("<leader>sf", builtin.find_files, { desc = "[s]earch [f]iles" })
-		map("<leader>ss", builtin.builtin, { desc = "[s]earch [s]elect Telescope" })
+		map("<leader>ss", builtin.lsp_dynamic_workspace_symbols, { desc = "[s]earch LSP [s]ymbols" })
 		map("<leader>sw", builtin.grep_string, { desc = "[s]earch current [w]ord" })
 		map("<leader>sg", builtin.live_grep, { desc = "[s]earch by [g]rep" })
 		map("<leader><leader>", builtin.live_grep, { desc = "[s]earch by [g]rep (alt keymap)" })
