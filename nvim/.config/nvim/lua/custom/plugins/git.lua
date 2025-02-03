d1e8daf18b711df1f151607923d176ba5173940f
Rihards Zauls <rihards.zauls@lmt.lv>
2025-02-03 09:23:15 +0200

    nvim - update deps

--- nvim/.config/nvim/lua/custom/plugins/git.lua
+++ nvim/.config/nvim/lua/custom/plugins/git.lua
@@ -11,21 +11,7 @@ return {
 			local neogit = require("neogit")
 			neogit.setup({})
 			vim.keymap.set("n", "<leader>gg", neogit.open, { desc = "Neo[g]it: open [g]it UI" })
-			vim.keymap.set("n", "<leader>gp", neogit.action("pull", "from_upstream"), { desc = "Neo[g]it: [p]ull upstream" })
-			vim.keymap.set("n", "<leader>gP", neogit.action("push", "to_upstream"), { desc = "Neo[g]it: [P]ush upstream" })
 		end,
 	},
-	{ -- Add git related signs to the gutter, as well as utilities for managing changes
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			-- signs = {
-			-- 	add = { text = "+" },
-			-- 	change = { text = "~" },
-			-- 	delete = { text = "_" },
-			-- 	topdelete = { text = "‾" },
-			-- 	changedelete = { text = "~" },
-			-- 	untracked = { text = "┆" },
-			-- },
-		},
-	},
+	"lewis6991/gitsigns.nvim", -- Add git related signs to the gutter, as well as utilities for managing changes
 }
