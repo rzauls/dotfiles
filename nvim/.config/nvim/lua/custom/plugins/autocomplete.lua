4b028a638e4562cd575a04b47d45a4f98a5354db
Rihards Zauls <rihards.zauls@gmail.com>
2025-03-29 13:15:08 +0200

    nvim - shove copilot suggestions into blink.cmp instead of polluting the screen with virtual text

--- nvim/.config/nvim/lua/custom/plugins/autocomplete.lua
+++ nvim/.config/nvim/lua/custom/plugins/autocomplete.lua
@@ -10,7 +10,10 @@ return {
 	{
 		"saghen/blink.cmp",
 		lazy = false, -- lazy loading handled internally
-		dependencies = "rafamadriz/friendly-snippets",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"giuxtaposition/blink-cmp-copilot",
+		},
 		-- use a release tag to download pre-built binaries
 		version = "*",
 
@@ -18,17 +21,62 @@ return {
 		---@type blink.cmp.Config
 		opts = {
 			keymap = { preset = "default" },
-
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer", "copilot" },
+				providers = {
+					copilot = {
+						name = "copilot",
+						module = "blink-cmp-copilot",
+						score_offset = 100,
+						async = true,
+						transform_items = function(_, items)
+							local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+							local kind_idx = #CompletionItemKind + 1
+							CompletionItemKind[kind_idx] = "Copilot"
+							for _, item in ipairs(items) do
+								item.kind = kind_idx
+							end
+							return items
+						end,
+					},
+				},
+			},
 			appearance = {
-				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
-				-- Useful for when your theme doesn't support blink.cmp
-				-- will be removed in a future release
 				use_nvim_cmp_as_default = true,
 				nerd_font_variant = "mono",
-			},
+				kind_icons = {
+					Copilot = "",
+					Text = "󰉿",
+					Method = "󰊕",
+					Function = "󰊕",
+					Constructor = "󰒓",
 
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
+					Field = "󰜢",
+					Variable = "󰆦",
+					Property = "󰖷",
+
+					Class = "󱡠",
+					Interface = "󱡠",
+					Struct = "󱡠",
+					Module = "󰅩",
+
+					Unit = "󰪚",
+					Value = "󰦨",
+					Enum = "󰦨",
+					EnumMember = "󰦨",
+
+					Keyword = "󰻾",
+					Constant = "󰏿",
+
+					Snippet = "󱄽",
+					Color = "󰏘",
+					File = "󰈔",
+					Reference = "󰬲",
+					Folder = "󰉋",
+					Event = "󱐋",
+					Operator = "󰪚",
+					TypeParameter = "󰬛",
+				},
 			},
 		},
 		-- allows extending the enabled_providers array elsewhere in your config
--- nvim/.config/nvim/lua/custom/plugins/copilot.lua
+++ nvim/.config/nvim/lua/custom/plugins/copilot.lua
@@ -7,72 +7,17 @@ local plugins = {
 			require("copilot").setup({
 				panel = {
 					enabled = false,
-					auto_refresh = false,
-					keymap = {
-						jump_prev = "[[",
-						jump_next = "]]",
-						accept = "<CR>",
-						refresh = "gr",
-						open = "<M-CR>",
-					},
-					layout = {
-						position = "bottom", -- | top | left | right | horizontal | vertical
-						ratio = 0.4,
-					},
 				},
 				suggestion = {
-					enabled = true,
-					auto_trigger = true,
-					hide_during_completion = true,
-					debounce = 75,
-					keymap = {
-						accept = "<D-Y>",
-						accept_word = false,
-						accept_line = false,
-						next = "<M-]>",
-						prev = "<M-[>",
-						dismiss = "<C-]>",
-					},
+					enabled = false,
 				},
-				filetypes = {
-					yaml = false,
-					markdown = false,
-					help = false,
-					gitcommit = false,
-					gitrebase = false,
-					hgcommit = false,
-					svn = false,
-					cvs = false,
-					["."] = false,
-				},
-				logger = {
-					print_log = false,
-					print_log_level = vim.log.levels.ERROR,
-				},
-				copilot_node_command = "node", -- Node.js version must be > 18.x
-				server_opts_overrides = {},
 			})
 		end,
 	},
 }
 
--- chat is broken on windows so we dont load even bother
+-- chat is broken on windows so we dont even bother loading it
 if not require("custom.util").is_windows then
-	-- hide copilot suggestions when blink.cmp menu is open
-	vim.api.nvim_create_autocmd("User", {
-		pattern = "BlinkCmpMenuOpen",
-		callback = function()
-			vim.b.copilot_suggestion_hidden = true
-		end,
-	})
-
-	vim.api.nvim_create_autocmd("User", {
-		pattern = "BlinkCmpMenuClose",
-		callback = function()
-			vim.b.copilot_suggestion_hidden = false
-		end,
-	})
-
 	table.insert(plugins, {
 		"CopilotC-Nvim/CopilotChat.nvim",
 		event = "VeryLazy",
@@ -84,5 +29,22 @@ if not require("custom.util").is_windows then
 		opts = {},
 	})
 end
+
+-- TODO: remove this if we end up staying on copilot suggestions inside blink.cmp
+--
+-- hide copilot suggestions when blink.cmp menu is open
+-- vim.api.nvim_create_autocmd("User", {
+-- 	pattern = "BlinkCmpMenuOpen",
+-- 	callback = function()
+-- 		vim.b.copilot_suggestion_hidden = true
+-- 	end,
+-- })
+--
+-- vim.api.nvim_create_autocmd("User", {
+-- 	pattern = "BlinkCmpMenuClose",
+-- 	callback = function()
+-- 		vim.b.copilot_suggestion_hidden = false
+-- 	end,
+-- })
 
 return plugins
