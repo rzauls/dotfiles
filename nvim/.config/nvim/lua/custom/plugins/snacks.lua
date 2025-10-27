fd526e886bd790fbed36ad631cd1f548e0ebf1dc
Rihards Zauls <rihards.zauls@catchbox.com>
2025-10-27 10:29:53 +0200

    nvim - update pickers, update deps

--- nvim/.config/nvim/lua/custom/plugins/snacks.lua
+++ nvim/.config/nvim/lua/custom/plugins/snacks.lua
@@ -6,6 +6,14 @@ return {
 		lazy = false,
 		---@type snacks.Config
 		opts = {
+			matcher = {
+				frecency = true,
+			},
+			formatters = {
+				file = {
+					truncate = "left",
+				},
+			},
 			picker = {
 				enabled = true,
 				layout = {
@@ -61,7 +69,13 @@ return {
 			--   { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
 			--   { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
 			--   { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
-			--   { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+			{
+				"<leader>fr",
+				function()
+					Snacks.picker.recent()
+				end,
+				desc = "Recent",
+			},
 			--   -- git
 			{
 				"<leader>gb",
@@ -84,13 +98,15 @@ return {
 				end,
 				desc = "Git Log Line",
 			},
-			--   { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
-			--   { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
-			--   { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
-			--   { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
 			--   -- Grep
 			--   { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
-			--   { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+			{
+				"<leader>sB",
+				function()
+					Snacks.picker.grep_buffers()
+				end,
+				desc = "Grep Open Buffers",
+			},
 			--   { "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
 			--   { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
 			--   -- search
@@ -128,7 +144,13 @@ return {
 				desc = "Help Pages",
 			},
 			--   { "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
-			--   { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
+			{
+				"<leader>si",
+				function()
+					Snacks.picker.icons()
+				end,
+				desc = "Icons",
+			},
 			--   { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
 			{
 				"<leader>sk",
@@ -155,7 +177,13 @@ return {
 				end,
 				desc = "Resume",
 			},
-			--   { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+			{
+				"<leader>su",
+				function()
+					Snacks.picker.undo()
+				end,
+				desc = "Undo History",
+			},
 			--   { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
 			--   -- LSP
 			{
